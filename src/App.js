f2875c33df990aea58152a1e51f55a379042ccdb
Jonathan Humphreys <jph.code@pm.me>
2021-11-15 18:17:23 +0000

    added page manager

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import PageManager from './pages/pageManager';
 
 function App() {
   return (
     <div className="App">
+      <PageManager />
     </div>
   );
 }
---
+++ src/pages/pageManager.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PageManager extends React.Component {
+    render() { 
+        return (
+            <div></div>
+        )
+    }
+}
+ 
+export default PageManager;
