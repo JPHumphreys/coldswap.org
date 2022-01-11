ff7290d4a9248a26fe000cfa4915e11b0329ce4a
DECENTRALISED <jph.code@pm.me>
2022-01-11 18:19:23 +0000

    added 2 error pages

---
+++ src/pages/error.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Error extends React.Component {
+    render() { 
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+ 
+export default Error;
---
+++ src/pages/notFoundError.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NotFoundError extends React.Component {
+    render() { 
+        return (
+        <div>
+            <h3>Page Not Found</h3>
+        </div>
+        );
+    }
+}
+ 
+export default NotFoundError;
